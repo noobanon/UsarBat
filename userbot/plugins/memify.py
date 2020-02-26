e5865c60a834faa86022cdf3a2f50064bf4884c3
noobanon <51384033+noobanon@users.noreply.github.com>
2020-02-27 02:47:05 +0530

    Update memify.py

--- userbot/plugins/memify.py
+++ userbot/plugins/memify.py
@@ -1,7 +1,7 @@
 """Reply to an image/sticker with .mmf` 'text on top' ; 'text on bottom
 base by: @r4v4n4
-created by: @A_Dark_Princ3
-if you change these, you gay.
+created by: @A_Dark_Princ3 The Gey Boi
+if you change these, you also gay.
 """
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
@@ -76,7 +76,7 @@ async def _(event):
           if response.text.startswith("Forward"):
               await event.edit("```can you kindly disable your forward privacy settings for good nibba?```")
           if "Okay..." in response.text:
-            await event.edit("```🤨 NANI?! This is not an image! This will take sum tym to convert to image owo 🧐```")
+            await event.edit("```NUB?! This is not an image! This will take sum tym to convert to image owo 🧐```")
             thumb = None
             if os.path.exists(thumb_image_path):
                 thumb = thumb_image_path
@@ -121,7 +121,7 @@ async def _(event):
                 # Courtesy: @A_Dark_Princ3
             )
             await event.delete()
-            await borg.send_message(event.chat_id, "`☠️☠️23 Points to Griffindor!🔥🔥`")
+            await borg.send_message(event.chat_id, "`Wait Nimbaa Lemme Finish This`")
           elif not is_message_image(reply_message):
             await event.edit("Invalid message type. Plz choose right message type u NIBBA.")
             return
