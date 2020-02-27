8a01f10239554952bc88844a780de3dbfce0af06
noobanon <51384033+noobanon@users.noreply.github.com>
2020-02-27 11:59:52 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -56,9 +56,9 @@ async def _(event):
         uploaded_sticker = await borg.upload_file(file, file_name=file_ext_ns_ion)
         packname = f"Jinga Stuxker"
         if userid == 631847622:
-            packshortname = "JingaStuxker"
+            packshortname = "JindaSticker"
         else:
-            packshortname = f"JindaStuxker" # format: Uni_Borg_userid
+            packshortname = f"JindaSticker" # format: Uni_Borg_userid
     elif not is_message_image(reply_message):
         await event.edit("Invalid message type")
         return
